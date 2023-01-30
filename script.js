48ab1216f138f2432269b8213189ebe51da86a1e
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-01-30 21:35:27 +0400

    add images - don`t work

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
 let json = `[{
     "title": "Stark",
+    "image": "img/Stark.jpg",
     "character": "Iron Man",
     "name": "Tony Stark",
-    "superpowers": "intelligence"
-},{
+    "superpowers": "intelligence",
+    },{
     "title": "Cap",
+    "image": "img/Cap.jpg",
     "character": "Сaptain America",
     "name": "Stephen Rogers",
     "superpowers": "strength, endurance, regeneration"
 },{
     "title": "Natasha",
+    "image": "img/Natasha.jpg",
     "character": "Black Widow",
     "name": "Natasha Romanoff",
     "superpowers": "spy"
 },{
     "title": "Thor",
+    "image": "img/Thor.jpg",
     "character": "Thor",
     "name": "Thor Odinson",
     "superpowers": "the power of a god, the hammer Mjolnir"
 },{
     "title": "Hawkeye",
+    "image": "img/Hawkeye.jpg",
     "character": "Hawkeye",
     "name": "Clint Barton",
     "superpowers": "great shooter"
 },{
     "title": "Banner",
+    "image": "img/Banner.jpg",
     "character": "Hulk",
     "name": "Bruce Banner",
     "superpowers": "intelligence, superhuman strength, flight"
@@ -35,9 +41,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     let cardsContent = "";
     for (let card of cards) {
-        cardsContent += 
-        `<div class='card'>
+        cardsContent += `<div class='card'>
             <h2>${card.title}</h2>
+            <img src="${card.image}" style="width: 300px" />
             <div>${card.character}</div>
             <div>${card.name}</div>
             <div>${card.superpowers}</div>
